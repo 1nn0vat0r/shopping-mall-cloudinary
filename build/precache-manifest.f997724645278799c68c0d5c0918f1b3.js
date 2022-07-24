93a02ad7260047969b03a3ad49970f9eb73147ca
1nn0vat0r <bsw8187@naver.com>
2022-07-24 13:30:22 +0000

    first commit

---
+++ build/precache-manifest.f997724645278799c68c0d5c0918f1b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f906a99e05452bac1a8258bf8322f4c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10fad3850af6662f5075",
+    "url": "/static/css/2.40e60438.chunk.css"
+  },
+  {
+    "revision": "08a9d54fd9aa97304182",
+    "url": "/static/css/main.7ef32e1b.chunk.css"
+  },
+  {
+    "revision": "10fad3850af6662f5075",
+    "url": "/static/js/2.9ccb871f.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/static/js/2.9ccb871f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08a9d54fd9aa97304182",
+    "url": "/static/js/main.f4030081.chunk.js"
+  },
+  {
+    "revision": "ceeacef460f662874725",
+    "url": "/static/js/runtime-main.60a1f217.js"
+  },
+  {
+    "revision": "880c1d2b03864dc7d44434ef35aed4b9",
+    "url": "/static/media/paulsplanetlogo.880c1d2b.png"
+  }
+]);
