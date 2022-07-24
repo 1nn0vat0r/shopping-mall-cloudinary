6eeefe338c092e95f243d5cd4deb118d09842546
1nn0vat0r <bsw8187@naver.com>
2022-07-24 14:30:07 +0000

    netlify commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'https://travelplanetserver.herokuapp.com/',
+            target: 'https://tripus-server.herokuapp.com/',
             changeOrigin: true,
             pathRewrite: {
                 '^/api': ''
